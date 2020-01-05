353c92644604f43e6c787407fe9c9872ac8d8300
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2020-01-05 15:49:07 +0100

    feature: add team classes

--- Team.py
+++ Team.py
@@ -1,7 +1,26 @@
 from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class TeamSeason:
+    season: str
+    wins: int
+    losses: int
+    pace: float
+    rel_pace: float
+    ortg: float
+    rel_ortg: float
+    drtg: float
+    rel_drtg: float
 
 
 @dataclass
 class Team:
     name: str
     code: str
+    seasons: List[TeamSeason]
+    wins: int
+    losses: int
+    championships: int
+    playoffs: int
+
