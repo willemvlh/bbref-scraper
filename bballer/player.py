873d29dc479b158cb7033ea91c31731c96e60624
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2020-01-19 17:29:40 +0100

    feature: add api call to get all players in a season, add typing info

--- bballer/player.py
+++ bballer/player.py
@@ -1,8 +1,12 @@
+from typing import List, Optional
+
+from bballer.models.player import Player
 from bballer.scrapers.PlayerPageScraper import PlayerPageScraper
 from bballer.scrapers.Search import Search
+from bballer.scrapers.misc import TotalMinutesScraper, BulkScraper
 
 
-def get_by_name(name: str):
+def get_by_name(name: str) -> Optional[Player]:
     result = Search.search_players(name)
     if not len(result):
         return None
@@ -10,14 +14,21 @@ def get_by_name(name: str):
     return PlayerPageScraper(url).player()
 
 
+def all_in_season(season: int) -> List[Player]:
+    url_scraper = TotalMinutesScraper(season)
+    urls = url_scraper.get_player_urls()
+    scraper = BulkScraper(urls)
+    return scraper.scrape_all()
+
+
 def search(term: str):
     return Search.search_players(term)
 
 
-def get_by_url(url: str):
+def get_by_url(url: str) -> Player:
     return PlayerPageScraper(url).player()
 
 
-def get_by_id(_id: str):
+def get_by_id(_id: str) -> Player:
     url = f"https://www.basketball-reference.com/players/{_id[0]}/{_id}.html"
     return PlayerPageScraper(url).player()
